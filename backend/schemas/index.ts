c30803a8f05fe5194b89ffbf65654e3b0ed4198c
Shabin-k <helloshabink@gmail.com>
2023-04-12 18:10:22 +0530

    code formating

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,7 +1,7 @@
-import pin from './pin';
-import user from './user';
-import comment from './comment';
-import postedBy from './postedBy';
-import save from './save';
+import pin from './pin'
+import user from './user'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
 
-export const schemaTypes = [user,pin,comment,postedBy,save]
+export const schemaTypes = [user, pin, comment, postedBy, save]
--- backend/schemas/user.js
+++ backend/schemas/user.js
@@ -1,17 +1,17 @@
 export default {
-    name: 'user',
-    title: 'User',
-    type: 'document',
-    fields: [
-      {
-        name: 'userName',
-        type: 'string',
-        title: 'UserName'
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image'
-      }
-    ]
-  }
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      type: 'string',
+      title: 'UserName',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+  ],
+}
