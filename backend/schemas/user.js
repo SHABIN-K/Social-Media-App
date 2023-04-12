21cc7c8f30fa5f732a57f3cc42ec48504157ee2c
Shabin-k <helloshabink@gmail.com>
2023-04-12 18:06:51 +0530

    backend completed

---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+      {
+        name: 'userName',
+        type: 'string',
+        title: 'UserName'
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image'
+      }
+    ]
+  }
