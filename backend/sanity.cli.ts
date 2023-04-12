1fe152e14b7f17cf1b6b94d4012997a68307c3ac
Shabin-k <helloshabink@gmail.com>
2023-04-12 17:24:20 +0530

    first commit

---
+++ backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'nx8gx5fq',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/index.ts
@@ -0,0 +1,2 @@
+export const schemaTypes = []
+export const sche = {}
