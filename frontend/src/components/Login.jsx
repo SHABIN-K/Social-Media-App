5719df2bf7956d544de5c6440b5ea3df5f70ab43
Shabin-k <helloshabink@gmail.com>
2023-04-13 17:42:24 +0530

    login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
-import GoogleLogin from 'react-google-login';
-import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
+import React, { useState, useEffect } from "react";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [user, setUser] = useState([]);
 
-export default Login
+  const responseGoogle = useGoogleLogin({
+    onSuccess: (tokenResponse) => setUser(tokenResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          localStorage.setItem("user", JSON.stringify(res.data));
+          const { id, picture, name } = res.data;
+          const doc = {
+            _id: id,
+            _type: 'user',
+            userName: name,
+            image: picture,
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const GoogleLoginButton = ({ onClick }) => {
+    return (
+      <button
+        onClick={onClick}
+        type="button"
+        className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+      >
+        <FcGoogle className="mr-4" /> Sign in with google
+      </button>
+    );
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          muted
+          autoPlay
+          controls={false}
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} alt="share me logo" width="130px" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLoginButton onClick={() => responseGoogle()} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import App from "./App";
 import "./index.css";
 
+const CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID_TOKEN;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
