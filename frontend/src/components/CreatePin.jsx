f46eac1d3b2f0ed1c4a27cd51e4e990f9379e97d
Shabin-k <helloshabink@gmail.com>
2023-04-17 14:36:16 +0530

    setup create pin

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+//from local files
+import { categories } from '../lib/categories';
+import { client } from '../lib/client';
+import Spinner from './Spinner';
+ 
 
 const CreatePin = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+  
   return (
     <div>CreatePin</div>
   )
