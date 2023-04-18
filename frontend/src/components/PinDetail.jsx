4defbf9458f0c72376b12e764c19a05d87120c4f
Shabin-k <helloshabink@gmail.com>
2023-04-18 12:47:52 +0530

    data👌😍

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
+import { MdDownloadForOffline } from 'react-icons/md';
+
+import { client, urlFor } from '../lib/client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../lib/data';
+import Spinner from './Spinner';
+
+const PinDetail = ({user}) => {
   return <div>PinDetail</div>;
 };
 
