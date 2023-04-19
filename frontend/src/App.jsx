d27420578cd40296d5e3f2dba285e9fcca8c37eb
Shabin-k <helloshabink@gmail.com>
2023-04-19 20:11:30 +0530

    fixed login problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { Home, Login } from "./pages";
+import { fetchUser } from "./lib/fetchUser";
 
 function App() {
+
+  useEffect(() => {
+    const User = fetchUser()
+
+    if (!User) navigate('/login');
+  }, []);
   return (
     <Routes>
       <Route path="*" element={<Home />} />
